e68534cd2d3dbc18138377e31442e8b1ae4e5b1d
ArpaSaha-AS <85447920+ArpaSaha-AS@users.noreply.github.com>
2021-09-15 12:18:08 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//Answer To The Question Number:1 start
+var countdown = $("#countdown").countdown360({
+  radius: 70.5,
+  strokeStyle: "#477050",
+  strokeWidth: undefined,
+  fillStyle: "#8ac575",
+  fontColor: "#477050", 
+  fontFamily: "sans-serif",
+  fontSize: undefined,
+  fontWeight: 700,
+  autostart: true,
+  seconds: 120,
+  label: ["second", "seconds"],
+  startOverAfterAdding: true, 
+  smooth: false,
+  onComplete: function () {}
+});
+//Answer To The Question Number:2 start
